b707260f30b07f15311f812ddf10a31f51c3ebc1
sayanthp <psayanth.p@gmail.com>
2024-09-20 21:34:18 +0530

    Project configurations updated

--- src/main/java/com/expentrack/eurekaservice/EurekaServerApplication.java
+++ src/main/java/com/expentrack/eurekaservice/EurekaServerApplication.java
@@ -2,12 +2,14 @@ package com.expentrack.eurekaservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-public class EurekaserviceApplication {
+@EnableEurekaServer
+public class EurekaServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EurekaserviceApplication.class, args);
+		SpringApplication.run(EurekaServerApplication.class, args);
 	}
 
 }
