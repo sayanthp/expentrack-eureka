cc1eab51c8ac80115a3a1265459d5f7b7d4b5f3f
sayanthp <psayanth.p@gmail.com>
2024-09-20 21:13:04 +0530

    Basic project created

---
+++ src/main/java/com/expentrack/eurekaservice/EurekaserviceApplication.java
@@ -0,0 +1,13 @@
+package com.expentrack.eurekaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaserviceApplication.class, args);
+	}
+
+}
